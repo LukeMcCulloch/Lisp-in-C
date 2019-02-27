f7a15115f0c50b4dc6874ed4785d80ff242b1557
Luke McCulloch <tlukemcculloch@gmail.com>
2019-02-27 00:17:20 -0600

    comments

--- src/hello_world.c
+++ src/hello_world.c
@@ -4,3 +4,10 @@ int main(int argc, char** argv) {
   puts("Hello, world!");
   return 0;
 }
+
+
+// gcc -std=c99 -Wall hello_world.c -o hello_world
+//
+// gcc hellow_world.c -o main
+//
+// https://www.recurse.com/blog/5-learning-c-with-gdb
